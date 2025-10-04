cb8bdffbc0f2e9c8c0e4d8eda95f7c7cfe0dde85
reocraft <reocraft.creeper@gmail.com>
2025-10-04 15:29:05 -0500

    Finished lab

--- src/main/java/edu/grinnell/csc207/genericslab/ArrayList.java
+++ src/main/java/edu/grinnell/csc207/genericslab/ArrayList.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 /**
  * An array-based implementation of a list.
  */
-public class ArrayList {
+public class ArrayList<T> {
 
     private static final int INITIAL_SIZE = 8;
-    private int[] data;
+    private T[] data;
     private int sz;
 
     /**
      * Constructs a new, empty array list.
      */
     public ArrayList() {
-        this.data = new int[INITIAL_SIZE];
+        this.data = (T[]) new Object[INITIAL_SIZE];
         this.sz = 0;
     }
 
@@ -30,7 +30,7 @@ public class ArrayList {
      * 
      * @param value the value to add to the end of the list
      */
-    public void add(int value) {
+    public void add(T value) {
         ensureCapacity();
         data[sz++] = value;
     }
@@ -46,7 +46,7 @@ public class ArrayList {
      * @param index the index of the element to retrieve
      * @return the value at the specified <code>index</code>
      */
-    public int get(int index) {
+    public T get(int index) {
         if (index < 0 || index >= sz) {
             throw new IndexOutOfBoundsException(index);
         }
@@ -59,11 +59,11 @@ public class ArrayList {
      * @param index the index of the element to remove
      * @return the element at <code>index</code>
      */
-    public int remove(int index) {
+    public T remove(int index) {
         if (index < 0 || index >= sz) {
             throw new IndexOutOfBoundsException(index);
         } else {
-            int ret = data[index];
+            T ret = data[index];
             for (int i = index; i < data.length - 1; i++) {
                 data[i] = data[i + 1];
             }
@@ -71,4 +71,51 @@ public class ArrayList {
             return ret;
         }
     }
+    /**
+     * mutates this array to have value sep in every other iteration.
+     * @param sep: the value that is being added in between every other iterataion on the arraylist.
+     */
+    public void intersperse(T sep) {
+        ArrayList<T> r = new ArrayList<>();
+        r.sz = this.sz * 2 - 1;
+        for (int i = 0; i < this.sz; i++) {
+            r.data[2 * i] = this.data[i];
+        }
+        for (int i = 1; i < this.sz; i++) {
+            r.data[1 + 2 * (i - 1)] = sep;
+        }
+        this.data = r.data;
+        this.sz = r.sz;
+    }
+
+    /**
+     * It is impossible to implement this method since we cannot find a "maximum" value in a String.
+     * @return exception
+     */
+    public T maximum() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns all the elements in the arraylist as a string.
+     * @returns string containing all the elements in the arraylist.
+     */
+    public String toString() {
+        String newString = "[";
+        for(int i = 0; i < this.sz - 1; i++){
+            newString += (String) this.data[i];
+            newString += ", ";
+        }
+        newString += (String) this.data[this.sz - 1];
+        return newString += "]";
+    }
+
+    /**
+     * It is impossible to compare differnt elements in the array if the types are not guarenteed.
+     * @return exception
+     */
+    public void insertionSort() {
+        throw new UnsupportedOperationException();
+    }
+
 }
--- src/main/java/edu/grinnell/csc207/genericslab/Optional.java
+++ src/main/java/edu/grinnell/csc207/genericslab/Optional.java
@@ -1,10 +1,54 @@
 package edu.grinnell.csc207.genericslab;
 
+import java.util.NoSuchElementException;
+
 /**
  * An Optional<@T> is either empty or holds a single value of type @T.
  * 
  * @param T the type of value potentially held by this <code>Optional</code>.
  */
+
+
+// Get returns a null when the value is null, but also when there is a type matching issue, which can lead to issues if we have a function that is checking
+// if a certain field is null, so the function would run and produce output that would be invalid, because the type isn't even correct.
+
 public class Optional<T> {
     /* TODO: fill me in with implementation! */  
+    private Optional() {}
+
+    static <T> Optional<T> empty() {
+        Optional<T> a = new Optional<>();
+        return a;
+    }
+
+    static <T> Optional<T> of(T value) {
+        Optional<T> b = (Optional <T>) value;
+        return b;
+    }
+
+    public boolean isEmpty() {
+        return this.isEmpty();
+    }
+
+    public boolean isPresent() {
+        return !this.isEmpty();
+    }
+
+    public T get() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        else {
+            return this.get();
+        }
+    }
+
+    public T orElse(T other) {
+        if (this.isPresent()) {
+            return this.get();
+        }
+        else {
+            return other;
+        }
+    }
 }
